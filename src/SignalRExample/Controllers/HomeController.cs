d961de3f679004f6faab7806aa1f4b33a5ac4dd1
ap-konovalov <a.konovalov@dodopizza.com>
2020-04-05 15:46:17 +0300

    add controller

---
+++ src/SignalRExample/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRExample.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HomeController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return File("~/index.html", "text/html");
+        }
+    }
+}
